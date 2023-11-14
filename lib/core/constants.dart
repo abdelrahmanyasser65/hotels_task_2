de4a267de9843f8e94095d1df2826a6c6bdf7bfa
abdelrahmanyasser65 <abdelrahman117ma@gmail.com>
2023-11-14 02:09:17 +0200

    add filters

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -8,3 +8,8 @@ List<String>sortBy=[
   "Price only",
   "Distance only"
 ];
+String star1="assets/images/star1.png";
+String star2="assets/images/star2.png";
+String star3="assets/images/star3.png";
+String star4="assets/images/star4.png";
+String star5="assets/images/star5.png";
--- lib/features/presentation/views/widget/filters_bottom_sheet.dart
+++ lib/features/presentation/views/widget/filters_bottom_sheet.dart
@@ -9,7 +9,7 @@ class FiltersBottomSheet extends StatefulWidget {
   @override
   State<FiltersBottomSheet> createState() => _FiltersBottomSheetState();
 }
-int indexG=0;
+double price=20;
 class _FiltersBottomSheetState extends State<FiltersBottomSheet> {
   @override
   Widget build(BuildContext context) {
@@ -20,7 +20,9 @@ class _FiltersBottomSheetState extends State<FiltersBottomSheet> {
             topLeft: Radius.circular(20),
             topRight: Radius.circular(20),
           ),
-          color: Colors.black.withOpacity(0.05)),
+         color: Colors.white.withOpacity(0.1)
+         // color: Colors.black.withOpacity(0.005)
+      ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -31,7 +33,9 @@ class _FiltersBottomSheetState extends State<FiltersBottomSheet> {
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(20),
                   topRight: Radius.circular(20),
+
                 ),
+
                 color: Colors.white),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -62,6 +66,188 @@ class _FiltersBottomSheetState extends State<FiltersBottomSheet> {
               ],
             ),
           ),
+          SizedBox(width: 1.h,),
+         Padding(
+           padding:  EdgeInsets.only(
+             top: 2.h,
+             left: 2.h,
+             right: 2.h,
+             bottom: 1.h
+           ),
+           child: Row(
+             children: [
+               CustomText(text: "PRICE PER NIGHT",
+                   color: Colors.black,
+                   fontSize: 12.sp,fontWeight: FontWeight.w600,),
+              const Spacer(),
+               Container(
+                 decoration: BoxDecoration(
+                   borderRadius: BorderRadius.circular(5),
+                   border: Border.all(
+                     color: Colors.black,
+                     width: 0.1.h
+                   ),
+
+                 ),
+                 padding: EdgeInsets.all(
+                   1.h
+                 ),
+                 child: Center(
+                   child: CustomText(
+                     text: "${price.round()}+ \$",
+                     color: Colors.black,
+                     fontSize: 14.sp,
+                     fontWeight: FontWeight.w400,
+                   ),
+                 ),
+               )
+             ],
+           ),
+         ),
+         Slider(
+           max:540,
+             min: 20,
+             thumbColor: Colors.white,
+             activeColor: Colors.blue.shade800,
+             inactiveColor: Colors.blue,
+             //divisions: 27,
+             label: price.round().toString(),
+             value: price,
+             onChanged: (val){
+               setState(() {
+                 price=val;
+               });
+             }),
+          Padding(
+            padding:  EdgeInsets.only(
+             left: 3.h,
+             right: 3.h,
+             bottom: 3.h,
+            ),
+            child: Row(
+              children: [
+                CustomText(text: "\$20",
+                    color: Colors.grey, fontSize: 12.sp),
+              const  Spacer(),
+                CustomText(text: "\$${price.round()}+",
+                    color: Colors.grey, fontSize: 12.sp),
+              ],
+            ),
+          ),
+          Padding(
+            padding:EdgeInsets.symmetric(
+              horizontal: 2.h
+            ),
+            child: CustomText
+              (text: "RATING", color: Colors.black,
+                fontSize: 12.sp,fontWeight: FontWeight.bold,),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 2.h,
+              vertical: 2.h
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                buildContainerRate(
+                  color: Colors.red.shade800,
+                  rate: 0
+                ),buildContainerRate(
+                  color: Colors.yellow.shade800,
+                  rate: 7
+                ),buildContainerRate(
+                  color: Colors.green.shade400,
+                  rate: 7.5
+                ),buildContainerRate(
+                  color: Colors.green.shade600,
+                  rate: 8
+                ),buildContainerRate(
+                  color: Colors.green.shade800,
+                  rate: 8.5
+                ),
+              ],
+            ),
+          ),
+
+          Padding(
+            padding:EdgeInsets.only(
+                left: 2.h,
+                right: 2.h,
+              top: 3.h,
+              bottom: 2.h
+            ),
+            child: CustomText
+              (text: "HOTEL CLASS", color: Colors.black,
+              fontSize: 12.sp,fontWeight: FontWeight.w600,),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: 2.h,
+
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                buildImageClass(image: star1),
+                buildImageClass(image:star2),
+                buildImageClass(image:star3),
+                buildImageClass(image:star4),
+                buildImageClass(image:star5),
+
+              ],
+            ),
+          ),
+
+
+          Padding(
+            padding:EdgeInsets.only(
+                left: 2.h,
+                right: 2.h,
+                top: 4.h,
+                bottom: 2.h
+            ),
+            child: CustomText
+              (text: "DISTANCE FROM", color: Colors.black,
+              fontSize: 12.sp,fontWeight: FontWeight.w600,),
+          ),
+          Container(
+            height: 0.1.h,
+            width: double.infinity,
+            color: Colors.grey,
+            margin: EdgeInsets.only(
+              left: 2.h,
+
+            ),
+          ),
+          Padding(
+            padding:EdgeInsets.all(
+                2.h
+            ),
+            child:
+            Row(
+              children: [
+                CustomText
+                  (text: "Location", color: Colors.black,
+                  fontSize: 12.sp,fontWeight: FontWeight.w500,),
+             const   Spacer(),
+                CustomText
+                  (text: "City Center", color: Colors.grey,
+                  fontSize: 14.sp,fontWeight: FontWeight.w400,),
+                Icon(Icons.arrow_forward_ios_outlined,color: Colors.grey,size: 2.h,)
+              ],
+            )
+          ),
+          Container(
+            height: 0.1.h,
+            width: double.infinity,
+            color: Colors.grey,
+            margin: EdgeInsets.only(
+              left: 2.h,
+
+            ),
+          ),
+
           const Spacer(),
           Container(
             padding: EdgeInsets.symmetric(
@@ -85,6 +271,38 @@ class _FiltersBottomSheetState extends State<FiltersBottomSheet> {
           )
         ],
       ),
+    );
+  }
+
+  InkWell buildImageClass({
+    image,onTap
+}) {
+    return
+       InkWell(
+      onTap:onTap,
+      child: Image.asset(image,height: 5.h,width: 5.h,fit: BoxFit.cover,));}
+
+  InkWell buildContainerRate({
+    color,rate,onTap
+}) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+                  decoration: BoxDecoration(
+                    color: color,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  height: 5.h,
+                  width: 5.h,
+                  child: Center(
+                    child: CustomText(
+                      text: "$rate+",
+                      color: Colors.white,
+                      fontSize: 12.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
     );
   }
 }
